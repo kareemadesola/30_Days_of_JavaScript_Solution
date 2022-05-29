13fbdecca80bb560248bb58d2eba31a2ac3453cb
kareemadesola <kareemadesola1999@gmail.com>
2022-05-29 19:55:53 +0100

    Exercise: Level 1

---
+++ 02_data_types/level_1.js
@@ -0,0 +1,75 @@
+// 1
+let challenge = '30 Days of JavaScript'
+// 2
+console.log('Number 2')
+console.log(challenge)
+// 3
+console.log('Number 3')
+console.log(challenge.length)
+// 4
+console.log('Number 4')
+console.log(challenge.toLocaleUpperCase())
+// 5
+console.log('Number 5')
+console.log(challenge.toLocaleLowerCase())
+// 6
+console.log('Number 6')
+console.log(challenge.substring(0,challenge.indexOf(' ')))
+console.log(challenge.substr(0, challenge.indexOf(' ')))
+// 7
+console.log('Number 7')
+console.log(challenge.substring(challenge.indexOf(' ')+ 1))
+// 8
+console.log('Number 8')
+console.log(challenge.includes('Script'))
+// 9
+console.log('Number 9')
+console.log(challenge.split(''))
+// 10
+console.log('Number 10')
+console.log(challenge.split(' '))
+// 11
+console.log('Number 11')
+console.log('Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'.split(', '))
+// 12
+console.log('NUmber 12')
+console.log(challenge.replace('JavaScript', 'Python'))
+// 13
+console.log('Number 13')
+console.log(challenge.charAt(15))
+// 14
+console.log('Number 14')
+console.log(challenge.charCodeAt(15))
+// 15
+console.log('Number 15')
+console.log(challenge.indexOf('a'))
+// 16
+console.log('Number 16')
+console.log(challenge.lastIndexOf('a'))
+// 17
+console.log('Number 17')
+console.log('You cannot end a sentence with because because because is a conjunction'.indexOf('because'))
+// 18
+console.log('Number 18')
+console.log('You cannot end a sentence with because because because is a conjunction'.lastIndexOf('because'))
+// 19
+console.log('Number 19')
+console.log(challenge.search('because'))
+// 20
+console.log('Number 20')
+console.log(challenge.trim())
+// 21
+console.log('Number 21')
+console.log(challenge.startsWith('30'))
+// 22
+console.log('Number 22')
+console.log(challenge.endsWith('ript'))
+// 23
+console.log('Number 23')
+console.log(challenge.match('a'))
+// 24
+console.log('Number 24')
+console.log('30 Days of '.concat('JavaScript'))
+// 25
+console.log('Number 25')
+console.log(challenge.repeat(2))
