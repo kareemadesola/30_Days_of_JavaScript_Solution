1e8cd53b2b5ca4ab3bf2adeed2151fcb8c349c03
kareemadesola <kareemadesola1999@gmail.com>
2022-05-31 14:07:55 +0100

    Exercise 03: Level 1 completed

---
+++ 03_booleans_operators_date/level_1.js
@@ -0,0 +1,59 @@
+console.log("Number 1");
+let firstName = "Adesola",
+  lastName = "Kareem",
+  country = "Nigeria",
+  city = "Lagos",
+  age = 12,
+  isMarried = true,
+  year = 2022;
+
+console.log("Number 2");
+console.log("10" === 10);
+
+console.log("Number 3");
+console.log(parseInt("9.8") === 10);
+
+console.log("Number 4i");
+console.log(0 < 45);
+console.log("rte".length === 3);
+console.log(typeof "rhe" == "string");
+
+console.log("Number 4 ii");
+console.log(45 > 123);
+console.log("rte".includes("rtede"));
+console.log(typeof "rhe" === "undefined");
+
+console.log("Number 5");
+console.log(4 > 3);
+console.log(4 >= 3);
+console.log(4 < 3);
+console.log(4 <= 3);
+console.log(4 == 4);
+console.log(4 === 4);
+console.log(4 != 4);
+console.log(4 == "4");
+console.log(4 === "4");
+console.log("python".length === "jargon".length);
+
+console.log("Number 6");
+console.log(4 > 3 && 10 < 12);
+console.log(4 > 3 || 10 > 12);
+console.log(4 > 3 || 10 > 12);
+console.log(4 > 3 || 10 > 12);
+console.log(4 > 3 || 10 > 12);
+console.log(!(4 > 3));
+console.log(!(4 < 3));
+console.log(!false);
+console.log(!(4 > 3) && 10 < 12);
+console.log(!(4 > 3) && 10 > 12);
+console.log(!(4 === "4"));
+console.log("dragon".includes("on") && "python".includes("on"));
+
+console.log("Number 7");
+const date = new Date();
+console.log(date.getFullYear());
+console.log(date.getMonth() + 1);
+console.log(date.getDate());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(Date.now());
