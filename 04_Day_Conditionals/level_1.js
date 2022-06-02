38b14fbaf67773311c3537ccb1dbd51b215fbce2
kareemadesola <kareemadesola1999@gmail.com>
2022-06-02 11:53:30 +0100

    Exercise 04: level_1.js completed

---
+++ 04_Day_Conditionals/level_1.js
@@ -0,0 +1,38 @@
+console.log("Number 1");
+// age = +prompt("Enter your age");
+// if (age >= 18) {
+//   alert("You are old enough to drive.");
+// } else {
+//   alert(`You are left with ${18 - age} years to drive`);
+// }
+
+console.log("Number 2");
+// yourAge = +prompt("Enter your age");
+// myAge = 22;
+// switch (true) {
+//   case yourAge > myAge:
+//     alert(`You are ${yourAge - myAge} older than me`);
+//     break;
+//   case myAge > yourAge:
+//     alert(`You are ${myAge - yourAge} older than me`);
+//     break;
+//   case myAge === yourAge:
+//     alert(`We are age mates`);
+// }
+
+console.log("Number 3");
+// console.log("Using if else");
+// let a = +prompt("Enter a");
+// let b = +prompt("Enter b");
+// if (a > b) console.log(`${a} is greater than ${b}`);
+// else console.log(`${a} is less than ${b}`);
+//
+// a > b
+//   ? console.log(`${a} is greater than b`)
+//   : console.log(`${a} is less than ${b}`);
+
+console.log("Number 4");
+// let number = +prompt("Enter a number");
+// number % 2 === 0
+//   ? alert(`${number} is an even number`)
+//   : alert(`${number} is an odd number`);
